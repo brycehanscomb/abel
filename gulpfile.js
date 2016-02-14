fdf82a2da3aebdee581426da614e0f740b19471b
Bryce Hanscomb <b.hanscomb@gmail.com>
2016-02-15 10:46:23 +1100

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var fs = require('fs');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+var rimraf = require('rimraf');
+var source = require('vinyl-source-stream');
+var _ = require('lodash');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var config = {
+	entryFile: './src/index.js',
+	outputDir: './lib/',
+	outputFile: 'abel.js'
+};
+
+// clean the output directory
+gulp.task('clean', function(cb){
+	rimraf(config.outputDir, cb);
+});
+
+var bundler;
+function getBundler() {
+	if (!bundler) {
+		bundler = watchify(browserify(config.entryFile, _.extend({ debug: true }, watchify.args)));
+	}
+	return bundler;
+};
+
+function bundle() {
+	return getBundler()
+		.transform(babelify)
+		.bundle()
+		.on('error', function(err) { console.log('Error: ' + err.message); })
+		.pipe(source(config.outputFile))
+		.pipe(gulp.dest(config.outputDir))
+		.pipe(reload({ stream: true }));
+}
+
+gulp.task('build-persistent', ['clean'], function() {
+	return bundle();
+});
+
+gulp.task('build', ['build-persistent'], function() {
+	process.exit(0);
+});
+
+gulp.task('watch', ['build-persistent'], function() {
+	getBundler().on('update', function() {
+		gulp.start('build-persistent')
+	});
+});
